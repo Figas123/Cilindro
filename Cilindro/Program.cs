c6cfe6e3b61e917fe2590d4e01b1f7a00a59dd65
Nuno <nunosilvares@hotmail.com>
2018-02-22 12:23:35 +0000

    Alteracoes na implementacao das variaveis

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -10,18 +10,22 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            double pi = 3.1415926;
+            // Fazer as variaveis e constantes
+            int a, r;
+            double volume, areaS;
+            string str;
+            const double pi = 3.1415926;
             // Pedir a altura do cilindro
             Console.WriteLine("Qual a altura do cilindro?");
-            string alt = Console.ReadLine();
-            int a = Convert.ToInt32(alt);
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
             // Pedir o raio do cilindro
             Console.WriteLine("\nQual o raio do cilindro?");
-            string raio = Console.ReadLine();
-            int r = Convert.ToInt32(raio);
+            str = Console.ReadLine();
+            r = Convert.ToInt32(str);
             // Calculos
-            double volume = pi * (r * r) * a;
-            double areaS = 2 * pi * r * (r + a);
+            volume = pi * (r * r) * a;
+            areaS = 2 * pi * r * (r + a);
             // Resultados
             Console.WriteLine("\nVolume = " + volume + "\n\nArea da Superficie = " + areaS + "\n");
         }
